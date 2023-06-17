2c93f164caf451e320fe8b867214a85face198bf
gigih <gigihfajrulrizky123@gmail.com>
2023-06-18 03:29:42 +0700

    push cam ke firestore

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import '../services/google_auth.dart';
@@ -9,23 +11,93 @@ class Profile extends StatefulWidget {
   final UserCredential userCredential;
   const Profile({super.key, required this.userCredential});
   
+  
 
   @override
   State<Profile> createState() => _ProfileState();
+  
 }
 
 class _ProfileState extends State<Profile> {
   
+  String imageUrl = '';
   File? _image;
+  bool showLocalFile = false;
 
-  Future<void> _getImageFromCamera() async {
-    final pickedImage = await ImagePicker().pickImage(source: ImageSource.camera);
-    if (pickedImage != null) {
-      setState(() {
-        _image = File(pickedImage.path);
-      });
-    }
-  }
+  
+
+  // Future<void> _getImageFromCamera() async {
+  //   final pickedImage = await ImagePicker().pickImage(source: ImageSource.camera);
+  //   if (pickedImage != null) {
+  //     setState(() {
+  //       _image = File(pickedImage.path);
+  //     });
+  //   }
+  // }
+
+//   Future<void> pickAndUploadImage() async {
+//   /*Langkah 1: Ambil gambar*/
+//   ImagePicker imagePicker = ImagePicker();
+//   XFile? file = await imagePicker.pickImage(source: ImageSource.camera);
+//   print('${file?.path}');
+
+//   if (file == null) return;
+
+//   /*Langkah 2: Unggah ke Firebase Storage*/
+//   var fileName = widget.userCredential.user!.email.toString() + '.jpg';
+//   Reference storageReference =
+//       FirebaseStorage.instance.ref().child('images').child(fileName);
+
+//   try {
+//     await storageReference.putFile(File(file.path));
+//     String imageUrl = await storageReference.getDownloadURL();
+//     print('Gambar diunggah: $imageUrl');
+
+//     /*Langkah 3: Kirim data gambar ke Firestore*/
+//     FirebaseFirestore firestore = FirebaseFirestore.instance;
+//     Map<String, dynamic> imageData = {
+//       'gambar': imageUrl,
+//     };
+
+//     // Kirim data gambar ke koleksi 'images' dengan dokumen baru yang secara otomatis dihasilkan ID-nya
+//     firestore.collection('images').add(imageData).then((DocumentReference document) {
+//       print('Data gambar berhasil dikirim dengan ID: ${document.id}');
+//     }).catchError((error) {
+//       print('Gagal mengirim data gambar: $error');
+//     });
+//   } catch (error) {
+//     print('Gagal mengunggah gambar: $error');
+//   }
+// }
+
+void pickAndUploadImage() async {
+  final image = await ImagePicker().pickImage(source: ImageSource.camera,
+    maxWidth: 100.0,
+    maxHeight: 100.0,
+  );
+
+  Reference ref = FirebaseStorage.instance.ref().child('profilepic.jpg');
+
+  await ref.putFile(File(image!.path));
+  ref.getDownloadURL().then((value) => {
+    print(value),
+    setState(() {
+      imageUrl = value;
+    })
+  });
+
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  CollectionReference profileCollection = firestore.collection('gambar');
+
+  Map<String, dynamic> dataToSend = {
+  'images': imageUrl,
+};
+
+profileCollection.add(dataToSend);
+
+}
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -82,12 +154,12 @@ class _ProfileState extends State<Profile> {
                   children: [
                     Padding(padding: EdgeInsets.only(top: 30.0),
                     child: 
-                      _image == null
+                      imageUrl == "" 
                       ? Image.asset("images/profile.png",
                         width: 100.0,
                         height: 100.0,
-                      ) : Image.file(
-                        _image!,
+                      ) : Image.network(
+                        imageUrl,
                         width: 100,
                         height: 100,
                       )
@@ -174,18 +246,37 @@ class _ProfileState extends State<Profile> {
             ),
             
                 Padding(padding: EdgeInsets.only(top: 750.0,left: 150.0),
-                    child:InkWell(
-              onTap: _getImageFromCamera,
-              child: SizedBox(
-                child: GestureDetector(
-                    onTap: _getImageFromCamera,
-                    child: Image.asset(
-                      'images/camera_icon.png',
-                      width: 100.0,
-                      height: 100.0,
-                    ),),
-              ),
-            ), 
+                    child:FutureBuilder(builder: 
+                      (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.waiting) {
+                           return CircularProgressIndicator();
+                        } else if (snapshot.hasError) {
+                          return Text('Error: ${snapshot.error}');
+                        } else {
+                          return InkWell(
+                            onTap: () {
+                              snapshot.connectionState != ConnectionState.waiting
+                                ? pickAndUploadImage()
+                                : null;
+                            },
+                            child: SizedBox(
+                              child: GestureDetector(
+                                onTap: () {
+                                  snapshot.connectionState != ConnectionState.waiting
+                                    ? pickAndUploadImage()
+                                    : null;
+                                },
+                                child: Image.asset(
+                                  'images/camera_icon.png',
+                                  height: 100.0,
+                                  width: 100.0,
+                                ),
+                              ),
+                            ),
+                          );
+                        }
+                      }
+                    )
             ),
 
           Container(
